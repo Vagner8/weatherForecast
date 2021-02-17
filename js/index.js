1764b2e429013836edcb24ee0a18ee33f24aa1c1
Dima_Vagner <myevropa1@gmail.com>
2021-02-17 16:03:24 +0200

    Added the google-script autocomplete and local state

--- js/index.js
+++ js/index.js
@@ -2,4 +2,22 @@ document.addEventListener("DOMContentLoaded", () => {
     const row = document.querySelector('.roof')
     const btn = document.querySelector('.btn')
     const input = document.querySelector('.form-control')
+
+    const autocomplete = new google.maps.places.Autocomplete(input)
+
+    const state = {
+        lat: '',
+        lon: '',
+        place: '',
+        cityName: '',
+        locales: navigator.language || navigator.userLanguage
+    }
+
+    google.maps.event.addListener(autocomplete, 'place_changed', () => {
+        buttonControl(btn, false)
+        state.place = autocomplete.getPlace()
+        state.cityName = state.place.name
+        state.lat = state.place.geometry.location.lat()
+        state.lon = state.place.geometry.location.lng()
+    })
 })
