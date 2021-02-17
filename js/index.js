113a452a02709de68371a16a63e3617190d2b40e
Dima_Vagner <myevropa1@gmail.com>
2021-02-17 15:59:02 +0200

    Added func DOMContentLoaded and got elements

---
+++ js/index.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const row = document.querySelector('.roof')
+    const btn = document.querySelector('.btn')
+    const input = document.querySelector('.form-control')
+})
